70302c22d9425e1225b2c7919dd79aa287c72b0d
Go Ohtani <otani_go@cyberagent.co.jp>
2015-10-14 18:43:51 +0900

    edit env related to golang

---
+++ .emacs.d/elpa/dash-20151006.221/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("dash.el") (22039 30081 826350 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
